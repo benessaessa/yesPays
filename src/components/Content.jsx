0921491185faa4fafbb88ae3bba3d423dc832ab8
MOHAMED ESSA <essa@pencil-designs.com>
2023-01-31 11:07:25 +0200

    edit pages

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import Products from './Products';
+import React from "react";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import Products from "./Products";
 const Content = () => {
-    return ( 
-        <div>
-            <div className='container pt-3'>
-                <Breadcrumb className='breadCrumb border-bottom mb-3'>
-                    <BreadcrumbItem>
-                    <a className='text-black font-size-14' href="#">
-                        Men
-                    </a>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem>
-                    <a className='text-black font-size-14' href="#">
-                    Clothing 
-                    </a>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem>
-                    <a className='text-black font-size-14' href="#">
-                    Tops  
-                    </a>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem>
-                    <a className='text-black font-size-14' href="#">
-                    Adidas 
-                    </a>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        T-Shirt
-                    </BreadcrumbItem>
-                </Breadcrumb>
-            </div>
-            <Products />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div className="container pt-3">
+        <Breadcrumb className="breadCrumb border-bottom mb-3">
+          <BreadcrumbItem>
+            <a className="text-black font-size-14" href="#">
+              Men
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <a className="text-black font-size-14" href="#">
+              Clothing
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <a className="text-black font-size-14" href="#">
+              Tops
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <a className="text-black font-size-14" href="#">
+              Adidas
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>T-Shirt</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <Products />
+    </div>
+  );
+};
+
 export default Content;
